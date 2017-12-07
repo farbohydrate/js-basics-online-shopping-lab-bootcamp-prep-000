2ce86f0661c9a19b3c7de5df70f3fe77b88924e8
Michael Farber <farbohydrate@gmail.com>
2017-12-07 00:47:50 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -58,6 +58,7 @@ function removeFromCart(item) {
 }
 
 function placeOrder(cardNumber) {
+  return cardNumber;
   if(cardNumber === null){
     console.log("Sorry, we don't have a credit card on file for you.");
   }
