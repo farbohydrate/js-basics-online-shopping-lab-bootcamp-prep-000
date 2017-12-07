974d7ee84b3e014ba00bc8e7347f74c4cd46b33e
Michael Farber <farbohydrate@gmail.com>
2017-12-07 00:26:03 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -50,7 +50,10 @@ function total() {
 function removeFromCart(item) {
   for(let i = 0; i < cart.length; i++){
     if(cart[i].hasOwnProperty){
-      
+      delete cart[i];
+      return cart;
+    } else {
+      return "That item is not in your cart.";
     }
   }
 }
